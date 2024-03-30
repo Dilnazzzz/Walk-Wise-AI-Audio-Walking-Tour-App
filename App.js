333b7338b3dbeb2a86a71904e769b2acd24819ab
Dilnaz Baltabayeva <76237763+Dilnazzzz@users.noreply.github.com>
2024-03-30 12:14:41 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useState, useEffect } from 'react';
-import { FIREBASE_AUTH } from './FirebaseConfig';
-import { User, onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_APP, FIREBASE_AUTH } from './FirebaseConfig';
+import { User, getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import HomeScreen from './screens/HomeScreen';
 import Discover from './screens/Discover';
@@ -16,6 +16,8 @@ import Map from './screens/Map';
 import MainContainer from './screens/MainContainer';
 import EditProfile from './screens/EditProfile';
 import TourScreen from './screens/TourScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import ResetPasswordScreen from './screens/ResetPasswordScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,30 +31,40 @@ export default function App() {
     });
   }, [])
 
+  // useEffect(() => {
+  //   onAuthStateChanged(FIREBASE_AUTH, (user) => {
+  //     if (user) {
+  //       // console.log('user', user);
+  //       setUser(user);
+  //     }
+  //   });
+  // }, [])
+
   return (
     <TailwindProvider>
       {/* <HomeScreen/> */}
       <NavigationContainer>
         <Stack.Navigator>
-          {user ? (<><Stack.Screen name="MainContainer" component={MainContainer} />
-            {/* // <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
-            {/* <Stack.Screen name="MainContainer" component={MainContainer} /> */}
-            <Stack.Screen name="Discover" component={Discover} />
-            <Stack.Screen name="Map" component={Map} />
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="ItemScreen" component={ItemScreen} />
-            <Stack.Screen name="EditProfile" component={EditProfile} />
-            <Stack.Screen name="TourScreen" component={TourScreen} />
-            </>)
-            :
-            <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          }
-          
-
+          {user ? (
+            <>
+              <Stack.Screen name="MainContainer" component={MainContainer} />
+              <Stack.Screen name="Discover" component={Discover} />
+              <Stack.Screen name="Map" component={Map} />
+              <Stack.Screen name="Profile" component={Profile} />
+              <Stack.Screen name="ItemScreen" component={ItemScreen} />
+              <Stack.Screen name="EditProfile" component={EditProfile} />
+              <Stack.Screen name="TourScreen" component={TourScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="HomeScreen" component={HomeScreen} />
+              <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+              <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
-
   );
 }
 
